e39472c02b84fae6b9bda3eec4f803d64fa7e830
unknown <rorchenko@gmail.com>
2022-09-24 15:19:10 +0300

    findchild removal

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export * from './useAppDispatch';
 export * from './useTypedSelector';
-export * from './useFindChild';
 export * from './useCloseDialog';
 
--- src/hooks/useConvertIntoBranch.ts
+++ src/hooks/useConvertIntoBranch.ts
@@ -4,16 +4,12 @@ import { MemberDto, RecursiveMember } from '../ts';
 
 export function useConvertIntoBranch() {
     const { members } = useTypedSelector(state => state.member);
-
-    const obj = {};
-
     const sorted = members.sort((m1, m2) => new Date(m1.birth).getTime() - new Date(m2.birth).getTime());
-
     const test = (obj: MemberDto): RecursiveMember | MemberDto => {
         if (obj.children.length === 0) return obj;
-        const children: (RecursiveMember | MemberDto)[] = [];
+        const children = [];
         for (let child of obj.children) {
-            const idx = members.findIndex(({ id }) => id === child.userId);
+            const idx = sorted.findIndex(({ id }) => id === child.userId);
             if (idx !== -1) {
                 children.push(test(members[idx]));
                 sorted.splice(idx, 1);
@@ -27,14 +23,4 @@ export function useConvertIntoBranch() {
             console.log(test(member));
         });
     }, [sorted]);
-
-    // for (let i = 0; i < sorted.length; i++) {
-    //     const branchId = members[i].branchId;
-    //     // @ts-ignore
-    //     obj[branchId] = obj[branchId] ? [...obj[branchId], members[i]] : [members[i]];
-    // }
-    //
-    // for (let key in obj) {
-    //
-    // }
 }
